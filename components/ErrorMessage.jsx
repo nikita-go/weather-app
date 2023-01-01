aa5a8984362821ee967b3c709befdf4639eef192
Nikita Gorshkov <gorshkovnikita@gmail.com>
2023-01-01 16:41:55 +0800

    Fix positioning

--- components/ErrorMessage.jsx
+++ components/ErrorMessage.jsx
@@ -2,10 +2,8 @@ import React from 'react';
 
 export default function ErrorMessage() {
     return (
-        <div className='relative flex justify-center top-10 z-40'>
-            <div className='flex justify-center items-center min-w-[50%] bg-[#ff5555] p-2 rounded'>
-                <p className='text-white text-lg'>No matching location found! Please enter a different location.</p>
-            </div>
+        <div className='absolute flex justify-center items-center top-7 w-[50%] bg-[#ff5555] p-2 rounded z-40'>
+            <p className='text-white text-lg'>No matching location found! Please enter a different location.</p>
         </div>
     )
 }
